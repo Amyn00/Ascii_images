e75906a998d9483e8e0e7eee49f4f3e1fcb1fb90
Amyn00 <medamine190@gmail.com>
2023-11-04 15:21:17 +0100

    update

--- asciicode.py
+++ asciicode.py
@@ -8,20 +8,17 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 # Define the allowed image formats
-allowed_formats = ["png", "jpg", "jpeg", "bmp"]
+allowed_formats = ["PNG", "JPG", "JPEG", "BMP"]
 
 # Get the image file path from the command-line argument
 img_path = sys.argv[1]
 
-# Open the image
-image = Image.open(img_path)
-"""
 # Check if the file format is allowed
 try:
-    with image as img:
-        # Get the format of the image (e.g., "png", "jpeg")
+    with Image.open(img_path) as img:
+        # Get the format of the image (e.g., "PNG", "JPEG")
         image_format = img.format
-    if image_format and image_format_upper() in allowed_formats:
+    if image_format and image_format.upper() in allowed_formats:
         print(f"Processing {image_format} image...")
     else:
         print(f"Error: Only {', '.join(allowed_formats)} image formats are allowed.")
@@ -29,7 +26,10 @@ try:
 except Exception as e:
     print("Error: Could not open the image file.")
     sys.exit(1)
-"""
+
+# Open the image
+image = Image.open(img_path)
+
 # Convert the image to grayscale
 image_grayscale = image.convert("L")
 print ("Break Point")
@@ -54,11 +54,11 @@ for pixel_value in pixels:
         ascii_image += '?'
 
 # Split the ASCII image into lines
-lignes_ascii = [ascii_image[i:i+largeur_cible] for i in range(0, len(ascii_image), largeur_cible)]
+lines_ascii = [ascii_image[i:i+largeur_cible] for i in range(0, len(ascii_image), largeur_cible)]
 
 # Write the ASCII image to a text file
-with open("image_ascii.txt", "w") as fichier:
-    for ligne in lignes_ascii:
-        fichier.write(ligne + "\n")
+with open("ascii_image.txt", "w") as fichier:
+    for line in lines_ascii:
+        fichier.write(line + "\n")
 
-print("The ASCII image has been saved to the image_ascii.txt file.")
+print("The ASCII image has been saved to the ascii_image.txt file.")
